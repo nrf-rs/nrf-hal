8285df84c12a236dbe672e881f2297fbfc82f9ab
Wez Furlong <wez@wezfurlong.org>
2018-08-25 16:32:02 -0700

    fixup a couple of warnings

--- nrf52840-hal/src/lib.rs
+++ nrf52840-hal/src/lib.rs
@@ -3,14 +3,12 @@
 #![cfg_attr(feature = "rt", feature(used))]
 #![feature(const_fn)]
 #![allow(non_camel_case_types)]
-#![feature(duration_extras)]
 
 extern crate bare_metal;
 extern crate cast;
 extern crate cortex_m;
 pub extern crate embedded_hal as hal;
 extern crate void;
-#[macro_use(block)]
 pub extern crate nb;
 pub extern crate nrf52840;
 
