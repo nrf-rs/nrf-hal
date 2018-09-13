jamesmunns/nrf52-hal

Rename pins to better match documentation
The product specification refers to single-digit pins as P0.00, P0.01,
etc.

src/gpio.rs
@@ -283,16 +283,16 @@ macro_rules! gpio {
 // 32-bit GPIO port (P0)
 // ===========================================================================
 gpio!(P0, p0, P0_Pin [
-    P0_0:  (p0_0,  0,  Input<Floating>),
-    P0_1:  (p0_1,  1,  Input<Floating>),
-    P0_2:  (p0_2,  2,  Input<Floating>),
-    P0_3:  (p0_3,  3,  Input<Floating>),
-    P0_4:  (p0_4,  4,  Input<Floating>),
-    P0_5:  (p0_5,  5,  Input<Floating>),
-    P0_6:  (p0_6,  6,  Input<Floating>),
-    P0_7:  (p0_7,  7,  Input<Floating>),
-    P0_8:  (p0_8,  8,  Input<Floating>),
-    P0_9:  (p0_9,  9,  Input<Floating>),
+    P0_0:  (p0_00,  0, Input<Floating>),
+    P0_1:  (p0_01,  1, Input<Floating>),
+    P0_2:  (p0_02,  2, Input<Floating>),
+    P0_3:  (p0_03,  3, Input<Floating>),
+    P0_4:  (p0_04,  4, Input<Floating>),
+    P0_5:  (p0_05,  5, Input<Floating>),
+    P0_6:  (p0_06,  6, Input<Floating>),
+    P0_7:  (p0_07,  7, Input<Floating>),
+    P0_8:  (p0_08,  8, Input<Floating>),
+    P0_9:  (p0_09,  9, Input<Floating>),
     P0_10: (p0_10, 10, Input<Floating>),
     P0_11: (p0_11, 11, Input<Floating>),
     P0_12: (p0_12, 12, Input<Floating>),

nrf-rs/nrf-hal

nrf52-hal-common/src/gpio.rs
@@ -283,16 +283,16 @@ macro_rules! gpio {
 // 32-bit GPIO port (P0)
 // ===========================================================================
 gpio!(P0, p0, P0_Pin [
-    P0_0:  (p0_0,  0,  Input<Floating>),
-    P0_1:  (p0_1,  1,  Input<Floating>),
-    P0_2:  (p0_2,  2,  Input<Floating>),
-    P0_3:  (p0_3,  3,  Input<Floating>),
-    P0_4:  (p0_4,  4,  Input<Floating>),
-    P0_5:  (p0_5,  5,  Input<Floating>),
-    P0_6:  (p0_6,  6,  Input<Floating>),
-    P0_7:  (p0_7,  7,  Input<Floating>),
-    P0_8:  (p0_8,  8,  Input<Floating>),
-    P0_9:  (p0_9,  9,  Input<Floating>),
+    P0_00: (p0_00,  0, Input<Floating>),
+    P0_01: (p0_01,  1, Input<Floating>),
+    P0_02: (p0_02,  2, Input<Floating>),
+    P0_03: (p0_03,  3, Input<Floating>),
+    P0_04: (p0_04,  4, Input<Floating>),
+    P0_05: (p0_05,  5, Input<Floating>),
+    P0_06: (p0_06,  6, Input<Floating>),
+    P0_07: (p0_07,  7, Input<Floating>),
+    P0_08: (p0_08,  8, Input<Floating>),
+    P0_09: (p0_09,  9, Input<Floating>),
     P0_10: (p0_10, 10, Input<Floating>),
     P0_11: (p0_11, 11, Input<Floating>),
     P0_12: (p0_12, 12, Input<Floating>),