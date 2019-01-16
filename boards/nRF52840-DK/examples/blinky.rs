e883670d76243401341ec586e5c33ff1ed8a5890
Noah Huesser <admin@yatekii.ch>
2019-01-17 00:08:42 +0100

    Basic implementation of the nRF52840-DK BSP -  needs docs - better examples needed - support for buttons needed

---
+++ boards/nRF52840-DK/examples/blinky.rs
@@ -0,0 +1,40 @@
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;
+use nb::block;
+
+#[allow(unused_imports)]
+use panic_semihosting;
+
+use nrf52840_dk_bsp::{
+    hal::{
+        prelude::*,
+        timer::Timer,
+    },
+    nRF52840DK,
+};
+
+
+#[entry]
+fn main() -> ! {
+    let mut nrf52 = nRF52840DK::take().unwrap();
+
+    let mut timer = nrf52.TIMER0.constrain();
+
+    // Alternately flash the red and blue leds
+    loop {
+        nrf52.leds.led_2.enable();
+        delay(&mut timer, 250_000); // 250ms
+        nrf52.leds.led_2.disable();
+        delay(&mut timer, 1_000_000); // 1s
+    }
+}
+
+fn delay<T>(timer: &mut Timer<T>, cycles: u32)
+where
+    T: TimerExt,
+{
+    timer.start(cycles);
+    let _ = block!(timer.wait());
+}
