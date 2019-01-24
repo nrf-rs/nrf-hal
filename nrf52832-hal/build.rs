a95321a257c8af844c90bd1e057f0f60580fc9d0
James Munns <james.munns@ferrous-systems.com>
2019-01-24 18:18:22 +0100

    Properly size the memory variants of the nrf53832 as per PS 1.4 table 135

--- nrf52832-hal/build.rs
+++ nrf52832-hal/build.rs
@@ -6,9 +6,22 @@ use std::path::PathBuf;
 fn main() {
     // Put the linker script somewhere the linker can find it
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+
+    let linker = match (cfg!(feature = "xxAA-package"), cfg!(feature = "xxAB-package")) {
+        (false, false) | (true, true) => {
+            panic!("\n\nMust select exactly one package for linker script generation!\nChoices: 'xxAA-package' or 'xxAB-package'\n\n");
+        }
+        (true, false) => {
+            include_bytes!("memory_xxAA.x")
+        }
+        (false, true) => {
+            include_bytes!("memory_xxAB.x")
+        }
+    };
+
     File::create(out.join("memory.x"))
         .unwrap()
-        .write_all(include_bytes!("memory.x"))
+        .write_all(linker)
         .unwrap();
     println!("cargo:rustc-link-search={}", out.display());
 
