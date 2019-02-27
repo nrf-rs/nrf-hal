5d57afa2204339a6668ba81a8bdeafe8d30b714c
Jonas Schievink <jonasschievink@gmail.com>
2019-02-27 01:16:37 +0100

    Clean up the RTFM example a bit

--- examples/rtfm-demo/src/main.rs
+++ examples/rtfm-demo/src/main.rs
@@ -8,18 +8,16 @@ use cortex_m_semihosting::{debug, hprintln};
 use rtfm::app;
 
 #[cfg(feature = "52810")]
-use nrf52810_pac;
+use nrf52810_hal as hal;
 
 #[cfg(feature = "52832")]
-use nrf52832_pac;
+use nrf52832_hal as hal;
 
 #[cfg(feature = "52840")]
-use nrf52840_pac;
+use nrf52840_hal as hal;
 
 
-#[cfg_attr(feature="52810", app(device = nrf52810_pac))]
-#[cfg_attr(feature="52832", app(device = nrf52832_pac))]
-#[cfg_attr(feature="52840", app(device = nrf52840_pac))]
+#[app(device = crate::hal::target)]
 const APP: () = {
     #[init]
     fn init() {
