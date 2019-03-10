1e7b9d1ef6bbadc48cd84df92212f7c3017cb8ae
James Munns <james.munns@ferrous-systems.com>
2019-03-10 04:05:26 +0100

    rustfmt spim.rs

--- nrf52-hal-common/src/spim.rs
+++ nrf52-hal-common/src/spim.rs
@@ -13,21 +13,12 @@ use core::iter::repeat_with;
 #[cfg(any(feature = "52832", feature = "52840"))]
 use crate::target::{SPIM1, SPIM2};
 
-use crate::target_constants::{EASY_DMA_SIZE, FORCE_COPY_BUFFER_SIZE};
+use crate::gpio::{Floating, Input, Output, Pin, PushPull};
 use crate::prelude::*;
-use crate::gpio::{
-    Pin,
-    Floating,
-    Input,
-    Output,
-    PushPull,
-};
-use crate::{
-    DmaSlice,
-    slice_in_ram,
-};
+use crate::target_constants::{EASY_DMA_SIZE, FORCE_COPY_BUFFER_SIZE};
+use crate::{slice_in_ram, DmaSlice};
 
-pub trait SpimExt : Deref<Target=spim0::RegisterBlock> + Sized {
+pub trait SpimExt: Deref<Target = spim0::RegisterBlock> + Sized {
     fn constrain(self, pins: Pins, frequency: Frequency, mode: Mode, orc: u8) -> Spim<Self>;
 }
 
@@ -46,10 +37,7 @@ macro_rules! impl_spim_ext {
 impl_spim_ext!(SPIM0,);
 
 #[cfg(any(feature = "52832", feature = "52840"))]
-impl_spim_ext!(
-    SPIM1,
-    SPIM2,
-);
+impl_spim_ext!(SPIM1, SPIM2,);
 
 /// Interface to a SPIM instance
 ///
@@ -59,15 +47,21 @@ impl_spim_ext!(
 ///   are disabled before using `Spim`. See product specification, section 15.2.
 pub struct Spim<T>(T);
 
-impl<T> embedded_hal::blocking::spi::Transfer<u8> for Spim<T> where T: SpimExt
+impl<T> embedded_hal::blocking::spi::Transfer<u8> for Spim<T>
+where
+    T: SpimExt,
 {
-   type Error = Error;
+    type Error = Error;
 
     fn transfer<'w>(&mut self, words: &'w mut [u8]) -> Result<&'w [u8], Error> {
         // Mask on segment where Data RAM is located on nrf52840 and nrf52832
         // Upper limit is choosen to entire area where DataRam can be placed
         let needs_copy = !slice_in_ram(words);
-        let chunk_sz = if needs_copy { EASY_DMA_SIZE } else { FORCE_COPY_BUFFER_SIZE };
+        let chunk_sz = if needs_copy {
+            EASY_DMA_SIZE
+        } else {
+            FORCE_COPY_BUFFER_SIZE
+        };
 
         for chunk in words.chunks(chunk_sz) {
             if needs_copy {
@@ -92,15 +86,21 @@ impl<T> embedded_hal::blocking::spi::Transfer<u8> for Spim<T> where T: SpimExt
         Ok(words)
     }
 }
-impl<T> embedded_hal::blocking::spi::Write<u8> for Spim<T> where T: SpimExt
+impl<T> embedded_hal::blocking::spi::Write<u8> for Spim<T>
+where
+    T: SpimExt,
 {
-   type Error = Error;
+    type Error = Error;
 
     fn write<'w>(&mut self, words: &'w [u8]) -> Result<(), Error> {
         // Mask on segment where Data RAM is located on nrf52840 and nrf52832
         // Upper limit is choosen to entire area where DataRam can be placed
         let needs_copy = !slice_in_ram(words);
-        let chunk_sz = if needs_copy { EASY_DMA_SIZE } else { FORCE_COPY_BUFFER_SIZE };
+        let chunk_sz = if needs_copy {
+            EASY_DMA_SIZE
+        } else {
+            FORCE_COPY_BUFFER_SIZE
+        };
 
         for chunk in words.chunks(chunk_sz) {
             if needs_copy {
@@ -114,18 +114,17 @@ impl<T> embedded_hal::blocking::spi::Write<u8> for Spim<T> where T: SpimExt
                     |_| {},
                 )?;
             } else {
-                self.do_spi_dma_transfer(
-                    DmaSlice::from_slice(chunk),
-                    DmaSlice::null(),
-                    |_| {},
-                )?;
+                self.do_spi_dma_transfer(DmaSlice::from_slice(chunk), DmaSlice::null(), |_| {})?;
             }
         }
 
         Ok(())
     }
 }
-impl<T> Spim<T> where T: SpimExt {
+impl<T> Spim<T>
+where
+    T: SpimExt,
+{
     pub fn new(spim: T, pins: Pins, frequency: Frequency, mode: Mode, orc: u8) -> Self {
         // Select pins
         spim.psel.sck.write(|w| {
@@ -136,40 +135,26 @@ impl<T> Spim<T> where T: SpimExt {
         });
 
         match pins.mosi {
-            Some(mosi) => {
-                spim.psel.mosi.write(|w| {
-                    let w = unsafe { w.pin().bits(mosi.pin) };
-                    #[cfg(feature = "52840")]
-                    let w = w.port().bit(mosi.port);
-                    w.connect().connected()
-                })
-            }
-            None =>{
-                spim.psel.mosi.write(|w| {
-                    w.connect().disconnected()
-                })
-            }
+            Some(mosi) => spim.psel.mosi.write(|w| {
+                let w = unsafe { w.pin().bits(mosi.pin) };
+                #[cfg(feature = "52840")]
+                let w = w.port().bit(mosi.port);
+                w.connect().connected()
+            }),
+            None => spim.psel.mosi.write(|w| w.connect().disconnected()),
         }
         match pins.miso {
-            Some(miso) => {
-                spim.psel.miso.write(|w| {
-                    let w = unsafe { w.pin().bits(miso.pin) };
-                    #[cfg(feature = "52840")]
-                    let w = w.port().bit(miso.port);
-                    w.connect().connected()
-                })
-            }
-            None => {
-                spim.psel.miso.write(|w| {
-                    w.connect().disconnected()
-                })
-            }
+            Some(miso) => spim.psel.miso.write(|w| {
+                let w = unsafe { w.pin().bits(miso.pin) };
+                #[cfg(feature = "52840")]
+                let w = w.port().bit(miso.port);
+                w.connect().connected()
+            }),
+            None => spim.psel.miso.write(|w| w.connect().disconnected()),
         }
 
         // Enable SPIM instance
-        spim.enable.write(|w|
-            w.enable().enabled()
-        );
+        spim.enable.write(|w| w.enable().enabled());
 
         // Configure mode
         spim.config.write(|w| {
@@ -186,9 +171,7 @@ impl<T> Spim<T> where T: SpimExt {
         });
 
         // Configure frequency
-        spim.frequency.write(|w|
-            w.frequency().variant(frequency)
-        );
+        spim.frequency.write(|w| w.frequency().variant(frequency));
 
         // Set over-read character to `0`
         spim.orc.write(|w|
@@ -206,7 +189,8 @@ impl<T> Spim<T> where T: SpimExt {
         rx: DmaSlice,
         mut cs_n: CSFun,
     ) -> Result<(), Error>
-        where CSFun: FnMut(bool)
+    where
+        CSFun: FnMut(bool),
     {
         let (tx_data_ptr, tx_len) = (tx.ptr, tx.len);
         let (rx_data_ptr, rx_len) = (rx.ptr, rx.len);
@@ -218,36 +202,33 @@ impl<T> Spim<T> where T: SpimExt {
         // set CS inactive
         cs_n(false);
         // Set up the DMA write
-        self.0.txd.ptr.write(|w|
-            unsafe { w.ptr().bits(tx_data_ptr) }
-        );
+        self.0
+            .txd
+            .ptr
+            .write(|w| unsafe { w.ptr().bits(tx_data_ptr) });
         self.0.txd.maxcnt.write(|w|
             // Note that that nrf52840 maxcnt is a wider
             // type than a u8, so we use a `_` cast rather than a `u8` cast.
             // The MAXCNT field is thus at least 8 bits wide and accepts the full
             // range of values that fit in a `u8`.
-            unsafe { w.maxcnt().bits(tx_len as _ ) }
-        );
+            unsafe { w.maxcnt().bits(tx_len as _ ) });
 
         // Set up the DMA read
         self.0.rxd.ptr.write(|w|
             // This is safe for the same reasons that writing to TXD.PTR is
             // safe. Please refer to the explanation there.
-            unsafe { w.ptr().bits(rx_data_ptr ) }
-        );
+            unsafe { w.ptr().bits(rx_data_ptr ) });
         self.0.rxd.maxcnt.write(|w|
             // This is safe for the same reasons that writing to TXD.MAXCNT is
             // safe. Please refer to the explanation there.
-            unsafe { w.maxcnt().bits(rx_len as _) }
-        );
+            unsafe { w.maxcnt().bits(rx_len as _) });
 
         // Set CS active
         cs_n(true);
         // Start SPI transaction
         self.0.tasks_start.write(|w|
             // `1` is a valid value to write to task registers.
-            unsafe { w.bits(1) }
-        );
+            unsafe { w.bits(1) });
 
         // Conservative compiler fence to prevent optimizations that do not
         // take in to account actions by DMA. The fence has been placed here,
@@ -286,11 +267,9 @@ impl<T> Spim<T> where T: SpimExt {
     pub fn read(
         &mut self,
         chip_select: &mut Pin<Output<PushPull>>,
-        tx_buffer  : &[u8],
-        rx_buffer  : &mut [u8],
-    )
-        -> Result<(), Error>
-    {
+        tx_buffer: &[u8],
+        rx_buffer: &mut [u8],
+    ) -> Result<(), Error> {
         self.transfer_split_uneven(chip_select, tx_buffer, rx_buffer)
     }
 
@@ -305,10 +284,8 @@ impl<T> Spim<T> where T: SpimExt {
     pub fn transfer(
         &mut self,
         chip_select: &mut Pin<Output<PushPull>>,
-        buffer     : &mut [u8],
-    )
-        -> Result<(), Error>
-    {
+        buffer: &mut [u8],
+    ) -> Result<(), Error> {
         ram_slice_check(buffer)?;
 
         for chunk in buffer.chunks(EASY_DMA_SIZE) {
@@ -321,7 +298,7 @@ impl<T> Spim<T> where T: SpimExt {
                     } else {
                         chip_select.set_high()
                     }
-                }
+                },
             )?;
         }
 
@@ -342,11 +319,9 @@ impl<T> Spim<T> where T: SpimExt {
     pub fn transfer_split_even(
         &mut self,
         chip_select: &mut Pin<Output<PushPull>>,
-        tx_buffer  : &[u8],
-        rx_buffer  : &mut [u8],
-    )
-        -> Result<(), Error>
-    {
+        tx_buffer: &[u8],
+        rx_buffer: &mut [u8],
+    ) -> Result<(), Error> {
         ram_slice_check(tx_buffer)?;
         ram_slice_check(rx_buffer)?;
 
@@ -355,17 +330,13 @@ impl<T> Spim<T> where T: SpimExt {
         let iter = txi.zip(rxi);
 
         for (t, r) in iter {
-            self.do_spi_dma_transfer(
-                DmaSlice::from_slice(t),
-                DmaSlice::from_slice(r),
-                |cs| {
-                    if cs {
-                        chip_select.set_low()
-                    } else {
-                        chip_select.set_high()
-                    }
+            self.do_spi_dma_transfer(DmaSlice::from_slice(t), DmaSlice::from_slice(r), |cs| {
+                if cs {
+                    chip_select.set_low()
+                } else {
+                    chip_select.set_high()
                 }
-            )?;
+            })?;
         }
 
         Ok(())
@@ -387,11 +358,9 @@ impl<T> Spim<T> where T: SpimExt {
     pub fn transfer_split_uneven(
         &mut self,
         chip_select: &mut Pin<Output<PushPull>>,
-        tx_buffer  : &[u8],
-        rx_buffer  : &mut [u8],
-    )
-        -> Result<(), Error>
-    {
+        tx_buffer: &[u8],
+        rx_buffer: &mut [u8],
+    ) -> Result<(), Error> {
         ram_slice_check(tx_buffer)?;
         ram_slice_check(rx_buffer)?;
         // For the tx and rx, we want to return Some(chunk)
@@ -410,30 +379,28 @@ impl<T> Spim<T> where T: SpimExt {
 
         // We then chain the iterators together, and once BOTH are feeding
         // back Nones, then we are done sending and receiving
-        let iters = txi.zip(rxi)
+        let iters = txi
+            .zip(rxi)
             .take_while(|(t, r)| t.is_some() && r.is_some())
-
             // We also turn the slices into either a DmaSlice (if there was data), or a null
             // DmaSlice (if there is no data)
             .map(|(t, r)| {
                 (
-                    t.map(|t| DmaSlice::from_slice(t)).unwrap_or_else(|| DmaSlice::null()),
-                    r.map(|r| DmaSlice::from_slice(r)).unwrap_or_else(|| DmaSlice::null()),
+                    t.map(|t| DmaSlice::from_slice(t))
+                        .unwrap_or_else(|| DmaSlice::null()),
+                    r.map(|r| DmaSlice::from_slice(r))
+                        .unwrap_or_else(|| DmaSlice::null()),
                 )
             });
 
         for (t, r) in iters {
-            self.do_spi_dma_transfer(
-                t,
-                r,
-                |cs| {
-                    if cs {
-                        chip_select.set_low()
-                    } else {
-                        chip_select.set_high()
-                    }
+            self.do_spi_dma_transfer(t, r, |cs| {
+                if cs {
+                    chip_select.set_low()
+                } else {
+                    chip_select.set_high()
                 }
-            )?;
+            })?;
         }
 
         Ok(())
@@ -444,16 +411,13 @@ impl<T> Spim<T> where T: SpimExt {
     /// This method uses the provided chip select pin to initiate the
     /// transaction, then transmits all bytes in `tx_buffer`. All incoming
     /// bytes are discarded.
-    pub fn write(&mut self,
+    pub fn write(
+        &mut self,
         chip_select: &mut Pin<Output<PushPull>>,
-        tx_buffer  : &[u8],
+        tx_buffer: &[u8],
     ) -> Result<(), Error> {
         ram_slice_check(tx_buffer)?;
-        self.transfer_split_uneven(
-            chip_select,
-            tx_buffer,
-            &mut [0u8; 0],
-        )
+        self.transfer_split_uneven(chip_select, tx_buffer, &mut [0u8; 0])
     }
 
     /// Return the raw interface to the underlying SPIM peripheral
