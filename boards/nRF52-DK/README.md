13502a3b7ceb502b0deec84f9f6d1bee50fba11d
Ruben Paz <me@ruben.io>
2019-03-13 00:42:00 +0000

    Add nRF52-DK board support crate

---
+++ boards/nRF52-DK/README.md
@@ -0,0 +1,6 @@
+# `nrf52-dk-bsp`
+
+Board support crate for the Nordic nRF52-DK
+https://www.nordicsemi.com/Software-and-Tools/Development-Kits/nRF52-DK
+
+This crate is in early development.
---
+++ boards/nRF52-DK/examples/blinky.rs
@@ -0,0 +1,30 @@
+#![no_std]
+#![no_main]
+
+extern crate cortex_m_rt as rt;
+extern crate panic_semihosting;
+extern crate nrf52_dk_bsp as dk;
+extern crate nb;
+
+use dk::{ Board, prelude::* };
+use rt::entry;
+use nb::block;
+
+#[entry]
+fn main() -> ! {
+    let mut board = Board::take().unwrap();
+
+    let mut timer = board.TIMER0.constrain();
+
+    let mut led_is_on = false;
+    loop {
+        if led_is_on {
+            board.leds.led_1.disable();
+        } else {
+            board.leds.led_1.enable();
+        }
+        timer.start(1_000_000_u32);
+        block!(timer.wait()).unwrap();
+        led_is_on = !led_is_on;
+    }
+}
