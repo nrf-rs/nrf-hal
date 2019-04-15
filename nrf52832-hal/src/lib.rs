a5019dad46a1bc031e1b87150f6582d270a20df3
Brian Cully <bjc@kublai.com>
2019-04-15 10:38:02 -0400

    Remove redundant pub use statements from nrf52832_hal::prelude.
    nrf52832-hal already pulls in the prelude from nrf52_hal_common, so
    these use statements were redundant.
    
    Export rtc::Rtc from nrf52832_hal

--- nrf52832-hal/src/lib.rs
+++ nrf52832-hal/src/lib.rs
@@ -7,20 +7,13 @@ pub use nrf52_hal_common::*;
 pub mod prelude {
     pub use crate::hal::prelude::*;
     pub use nrf52_hal_common::prelude::*;
-
-    pub use crate::clocks::ClocksExt;
-    pub use crate::gpio::GpioExt;
-    pub use crate::spim::SpimExt;
-    pub use crate::time::U32Ext;
-    pub use crate::timer::TimerExt;
-    pub use crate::uarte::UarteExt;
-    pub use crate::saadc::SaadcExt;
 }
 
 pub use crate::clocks::Clocks;
 pub use crate::delay::Delay;
+pub use crate::rtc::Rtc;
 pub use crate::saadc::Saadc;
 pub use crate::spim::Spim;
+pub use crate::temp::Temp;
 pub use crate::timer::Timer;
 pub use crate::uarte::Uarte;
-pub use crate::temp::Temp;
