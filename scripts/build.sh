2504eae708e2cd441d1175e96bcf7d4f190fcc09
Hanno Braun <hb@hannobraun.de>
2019-04-28 15:13:13 +0200

    Add missing crate to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,7 @@ cargo build --manifest-path nrf52810-hal/Cargo.toml --target thumbv7em-none-eabi
 cargo build --manifest-path nrf52832-hal/Cargo.toml
 cargo build --manifest-path nrf52840-hal/Cargo.toml
 cargo build --manifest-path boards/adafruit_nrf52pro/Cargo.toml --examples
+cargo build --manifest-path boards/adafruit-nrf52-bluefruit-le/Cargo.toml --examples
 cargo build --manifest-path boards/nRF52-DK/Cargo.toml --examples
 cargo build --manifest-path boards/nRF52840-DK/Cargo.toml --examples
 cargo build --manifest-path examples/rtfm-demo/Cargo.toml
