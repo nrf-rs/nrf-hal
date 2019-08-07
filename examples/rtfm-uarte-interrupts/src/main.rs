65f56b2ceb80aa20cbf1bb8ba0ad3b788e8dfbaf
Emil Fresk <emil.fresk@gmail.com>
2019-08-07 15:52:08 +0200

    Use release RTFM version

--- examples/rtfm-uarte-interrupts/src/main.rs
+++ examples/rtfm-uarte-interrupts/src/main.rs
@@ -34,21 +34,21 @@ const APP: () = {
     static mut PRODUCER: Producer<'static, Box<DMAPool>, TXQSize> = ();
 
     #[init(spawn = [])]
-    fn init(c: init::Context) -> init::LateResources {
+    fn init() -> init::LateResources {
         // for the actual DMA buffers
         static mut MEMORY: [u8; DMA_MEM] = [0; DMA_MEM];
         // for the producer/consumer of TX
-        static mut TX_RB: Option<Queue<Box<DMAPool>, TXQSize>> = None;
+        static mut TX_RB: Queue<Box<DMAPool>, TXQSize> = Queue(heapless::i::Queue::new());
 
         hprintln!("init").unwrap();
         // move MEMORY to P (the DMA buffer allocator)
         DMAPool::grow(MEMORY);
 
-        let port0 = p0::Parts::new(c.device.P0);
+        let port0 = p0::Parts::new(device.P0);
 
         //adafruit nrf52 le
         let uarte0 = Uarte::new(
-            c.device.UARTE0,
+            device.UARTE0,
             uarte::Pins {
                 txd: port0.p0_06.into_push_pull_output(Level::High).degrade(),
                 rxd: port0.p0_08.into_floating_input().degrade(),
@@ -59,8 +59,7 @@ const APP: () = {
             uarte::Baudrate::BAUD115200,
         );
 
-        *TX_RB = Some(Queue::new());
-        let (txp, txc) = TX_RB.as_mut().unwrap().split();
+        let (txp, txc) = TX_RB.split();
         let (rx, tx) = uarte0.split(Queue::new(), txc);
 
         init::LateResources {
@@ -72,38 +71,38 @@ const APP: () = {
 
     // // we can get Box<P> us being now the owner
     #[task(capacity = 2, resources = [PRODUCER])]
-    fn printer(c: printer::Context, data: Box<DMAPool>) {
+    fn printer(data: Box<DMAPool>) {
         // enqueue a test message
         // let mut b = DMAPool::alloc().unwrap().freeze();
         // b.copy_from_slice(&[0, 1, 2, 3]);
 
         // hprintln!("{:?}", &data).unwrap();
         // just do the buffer dance without copying
-        c.resources.PRODUCER.enqueue(data).unwrap();
+        resources.PRODUCER.enqueue(data).unwrap();
         rtfm::pend(interrupt::UARTE0_UART0);
     }
 
     #[task]
-    fn rx_error(_: rx_error::Context, err: RXError) {
+    fn rx_error(err: RXError) {
         hprintln!("rx_error {:?}", err).unwrap();
     }
 
     #[interrupt(priority = 2, resources = [RX, TX], spawn = [printer, rx_error])]
-    fn UARTE0_UART0(c: UARTE0_UART0::Context) {
+    fn UARTE0_UART0() {
         // probe RX
-        match c.resources.RX.process_interrupt() {
+        match resources.RX.process_interrupt() {
             Ok(Some(b)) => {
                 // delegate data to printer
-                match c.spawn.printer(b) {
-                    Err(_) => c.spawn.rx_error(RXError::OOM).unwrap(),
+                match spawn.printer(b) {
+                    Err(_) => spawn.rx_error(RXError::OOM).unwrap(),
                     _ => (),
                 };
             }
             Ok(None) => (), // no
-            Err(err) => c.spawn.rx_error(err).unwrap(),
+            Err(err) => spawn.rx_error(err).unwrap(),
         }
 
-        c.resources.TX.process_interrupt();
+        resources.TX.process_interrupt();
     }
 
     extern "C" {
