67d9a62c2193da8151688de3ff72c20cdc58886a
Jamie McClymont <jamie@kwiius.com>
2019-11-29 05:29:49 +1300

    Update to svd2rust 0.16-based PACs

--- nrf52-hal-common/src/spim.rs
+++ nrf52-hal-common/src/spim.rs
@@ -11,7 +11,7 @@ use crate::target::{spim0_ns as spim0, SPIM0_NS as SPIM0};
 use crate::target::{spim0, SPIM0};
 
 pub use embedded_hal::spi::{Mode, Phase, Polarity, MODE_0, MODE_1, MODE_2, MODE_3};
-pub use spim0::frequency::FREQUENCYW as Frequency;
+pub use spim0::frequency::FREQUENCY_A as Frequency;
 
 use core::iter::repeat_with;
 
