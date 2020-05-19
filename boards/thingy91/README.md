72d6ef7be0c2feee059f39e7fc7bbeaa3be99791
Jonathan Pallant (42 Technology) <jonathan.pallant@42technology.com>
2020-05-19 15:53:54 +0100

    Add Thingy:91 support (at least for the nRF9160 SiP on there).

---
+++ boards/thingy91/README.md
@@ -0,0 +1,9 @@
+# `thingy91-nrf9160-bsp`
+
+Board support crate for the nRF9160 on the Nordic Thingy:91.
+
+https://www.nordicsemi.com/Software-and-tools/Prototyping-platforms/Nordic-Thingy-91
+
+The Thingy:91 also contains a nRF52840. This crate assumes you are running the default nRF52840 firmware, as supplied out of the box.
+
+This crate is in early development.
---
+++ boards/thingy91/examples/blinky.rs
@@ -0,0 +1,33 @@
+#![no_std]
+#![no_main]
+
+extern crate cortex_m_rt as rt;
+extern crate nb;
+extern crate panic_semihosting;
+extern crate thingy91_bsp as bsp;
+
+use bsp::{hal::Timer, prelude::*, Board};
+use core::fmt::Write;
+use nb::block;
+use rt::entry;
+
+#[entry]
+fn main() -> ! {
+    let mut board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0_NS);
+
+    writeln!(board.uart0, "Hello, UART0!").unwrap();
+    writeln!(board.uart1, "Hello, UART1!").unwrap();
+
+    let mut led_is_on = false;
+    loop {
+        if led_is_on {
+            board.leds.red.disable();
+        } else {
+            board.leds.red.enable();
+        }
+        timer.start(1_000_000_u32);
+        block!(timer.wait()).unwrap();
+        led_is_on = !led_is_on;
+    }
+}
