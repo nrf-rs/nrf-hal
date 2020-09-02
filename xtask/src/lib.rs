c2bcf1e27d01137b28f8472a7ff96272fec93b49
Jonas Schievink <jonas.schievink@ferrous-systems.com>
2020-09-02 16:59:36 +0200

    Use cargo-xtask in CI

---
+++ xtask/src/lib.rs
@@ -0,0 +1,63 @@
+use std::process::Command;
+
+pub static HALS: &[(&str, &str)] = &[
+    ("nrf51-hal", "thumbv6m-none-eabi"),
+    ("nrf9160-hal", "thumbv8m.main-none-eabihf"),
+    ("nrf52810-hal", "thumbv7em-none-eabi"),
+    ("nrf52832-hal", "thumbv7em-none-eabihf"),
+    ("nrf52833-hal", "thumbv7em-none-eabihf"),
+    ("nrf52840-hal", "thumbv7em-none-eabihf"),
+];
+
+pub static EXAMPLES: &[(&str, &[&str])] = &[
+    ("ccm-demo", &["52810", "52832", "52833", "52840"]),
+    ("comp-demo", &[]),
+    ("ecb-demo", &["51", "52810", "52832", "52833", "52840"]),
+    ("gpiote-demo", &[]),
+    ("i2s-controller-demo", &[]),
+    ("lpcomp-demo", &[]),
+    ("ppi-demo", &["51", "52810", "52832", "52833", "52840"]),
+    ("pwm-demo", &[]),
+    ("qdec-demo", &[]),
+    ("rtic-demo", &["51", "52810", "52832", "52840"]),
+    ("spi-demo", &[]),
+    ("twi-ssd1306", &["52832", "52840"]),
+    ("twim-demo", &[]),
+    ("twis-demo", &[]),
+    ("wdt-demo", &[]),
+];
+
+pub fn feature_to_target(feat: &str) -> &str {
+    match feat {
+        "51" => "thumbv6m-none-eabi",
+        "52810" => "thumbv7em-none-eabi",
+        _ if feat.starts_with("52") => "thumbv7em-none-eabihf",
+        _ => panic!("unknown Cargo feature `{}`", feat),
+    }
+}
+
+pub fn install_targets() {
+    let mut targets = HALS
+        .iter()
+        .map(|(_, target)| *target)
+        .chain(
+            EXAMPLES
+                .iter()
+                .flat_map(|(_, features)| features.iter().map(|feat| feature_to_target(feat))),
+        )
+        .collect::<Vec<_>>();
+    targets.sort();
+    targets.dedup();
+
+    let mut cmd = Command::new("rustup");
+    cmd.args(&["target", "add"]).args(&targets);
+    let status = cmd
+        .status()
+        .map_err(|e| format!("couldn't execute {:?}: {}", cmd, e))
+        .unwrap();
+    assert!(
+        status.success(),
+        "failed to install targets with rustup: {:?}",
+        cmd
+    );
+}
---
+++ xtask/tests/ci.rs
@@ -0,0 +1,76 @@
+use std::env;
+use std::{fs, process::Command};
+use xtask::{EXAMPLES, HALS};
+
+fn build_example(example: &str, target: &str, feature: Option<&str>) {
+    println!("building `{}` for `{}`", example, target);
+    let mut cargo = Command::new("cargo");
+    let toml_path = format!("examples/{}/Cargo.toml", example);
+    cargo.args(&["build", "--target", target, "--manifest-path", &toml_path]);
+    if let Some(feature) = feature {
+        cargo.args(&["--features", feature]);
+    }
+
+    let status = cargo
+        .status()
+        .map_err(|e| format!("could not execute {:?}: {}", cargo, e))
+        .unwrap();
+    assert!(
+        status.success(),
+        "command exited with error status: {:?}",
+        cargo
+    );
+}
+
+fn main() {
+    xtask::install_targets();
+
+    // We execute from the `xtask` dir, so `cd ..` so that we can find `examples` etc.
+    env::set_current_dir("..").unwrap();
+
+    // Build-test every HAL.
+    for (hal, target) in HALS {
+        let mut cargo = Command::new("cargo");
+        let toml_path = format!("{}/Cargo.toml", hal);
+        let status = cargo
+            .args(&["build", "--manifest-path", &toml_path, "--target", target])
+            .status()
+            .map_err(|e| format!("could not execute {:?}: {}", cargo, e))
+            .unwrap();
+        assert!(
+            status.success(),
+            "command exited with error status: {:?}",
+            cargo
+        );
+    }
+
+    // Build-test every example with each supported feature.
+    for (example, features) in EXAMPLES {
+        // Features are exclusive (they select the target chip), so we test each one
+        // individually.
+        if features.is_empty() {
+            // Use a default target.
+            build_example(example, "thumbv7em-none-eabihf", None);
+        } else {
+            for feature in *features {
+                let target = xtask::feature_to_target(feature);
+                build_example(example, target, Some(*feature));
+            }
+        }
+    }
+
+    // Ensure that no examples get added without an entry in EXAMPLES.
+    for entry in fs::read_dir("examples").unwrap() {
+        let entry = entry.unwrap();
+        let name = entry.file_name();
+        let name = name.to_str().unwrap();
+
+        if EXAMPLES
+            .iter()
+            .find(|(example, ..)| *example == name)
+            .is_none()
+        {
+            panic!("example `{}` is missing an entry in xtask `EXAMPLES`", name);
+        }
+    }
+}
