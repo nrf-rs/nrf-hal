38ade73b6a4f32283b95f418aac550e46868ea48
Jonas Schievink <jonasschievink@gmail.com>
2021-02-14 23:37:13 +0100

    Revert "don't return PollResult::Data when there's no data"
    This reverts commit 5c88d4e02134ba7227c9d9a99d2c0636242cedf3.

--- nrf-hal-common/src/usbd/mod.rs
+++ nrf-hal-common/src/usbd/mod.rs
@@ -625,14 +625,10 @@ impl UsbBus for Usbd {
 
             // TODO: Check ISO EP
 
-            if out_complete != 0 || in_complete != 0 || ep_setup != 0 {
-                PollResult::Data {
-                    ep_out: out_complete,
-                    ep_in_complete: in_complete,
-                    ep_setup,
-                }
-            } else {
-                PollResult::None
+            PollResult::Data {
+                ep_out: out_complete,
+                ep_in_complete: in_complete,
+                ep_setup,
             }
         })
     }
