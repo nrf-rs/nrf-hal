f55b5418bad6aee69451c096c9ff74832abc6f8a
Vadim Kaushan <admin@disasm.info>
2021-06-05 15:22:31 +0300

    Add serial and test_class examples

---
+++ examples/usb/examples/serial.rs
@@ -0,0 +1,79 @@
+#![no_std]
+#![no_main]
+
+use panic_semihosting as _;
+
+use cortex_m_rt::entry;
+use nrf52840_hal::usbd::Usbd;
+use nrf52840_hal::clocks::Clocks;
+use nrf52840_pac::Peripherals;
+use usb_device::device::{UsbDeviceBuilder, UsbVidPid};
+use usbd_serial::{SerialPort, USB_CLASS_CDC};
+
+
+#[entry]
+fn main() -> ! {
+    let periph = Peripherals::take().unwrap();
+    while !periph
+        .POWER
+        .usbregstatus
+        .read()
+        .vbusdetect()
+        .is_vbus_present()
+    {}
+
+    // wait until USB 3.3V supply is stable
+    while !periph
+        .POWER
+        .events_usbpwrrdy
+        .read()
+        .events_usbpwrrdy()
+        .bit_is_clear()
+    {}
+
+    let clocks = Clocks::new(periph.CLOCK);
+    let clocks = clocks.enable_ext_hfosc();
+
+    let usbd = periph.USBD;
+
+    let usb_bus = Usbd::new(usbd, &clocks);
+    let mut serial = SerialPort::new(&usb_bus);
+
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .manufacturer("Fake company")
+        .product("Serial port")
+        .serial_number("TEST")
+        .device_class(USB_CLASS_CDC)
+        .max_packet_size_0(64) // (makes control transfers 8x faster)
+        .build();
+
+    loop {
+        if !usb_dev.poll(&mut [&mut serial]) {
+            continue;
+        }
+
+        let mut buf = [0u8; 64];
+
+        match serial.read(&mut buf) {
+            Ok(count) if count > 0 => {
+                // Echo back in upper case
+                for c in buf[0..count].iter_mut() {
+                    if 0x61 <= *c && *c <= 0x7a {
+                        *c &= !0x20;
+                    }
+                }
+
+                let mut write_offset = 0;
+                while write_offset < count {
+                    match serial.write(&buf[write_offset..count]) {
+                        Ok(len) if len > 0 => {
+                            write_offset += len;
+                        },
+                        _ => {},
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+}
---
+++ examples/usb/examples/test_class.rs
@@ -0,0 +1,47 @@
+#![no_std]
+#![no_main]
+
+use panic_semihosting as _;
+
+use cortex_m_rt::entry;
+use nrf52840_hal::usbd::Usbd;
+use nrf52840_hal::clocks::Clocks;
+use nrf52840_pac::Peripherals;
+use usb_device::test_class::TestClass;
+
+#[entry]
+fn main() -> ! {
+    let periph = Peripherals::take().unwrap();
+    while !periph
+        .POWER
+        .usbregstatus
+        .read()
+        .vbusdetect()
+        .is_vbus_present()
+    {}
+
+    // wait until USB 3.3V supply is stable
+    while !periph
+        .POWER
+        .events_usbpwrrdy
+        .read()
+        .events_usbpwrrdy()
+        .bit_is_clear()
+    {}
+
+    let clocks = Clocks::new(periph.CLOCK);
+    let clocks = clocks.enable_ext_hfosc();
+
+    let usbd = periph.USBD;
+    let usb_bus = Usbd::new(usbd, &clocks);
+
+    let mut test = TestClass::new(&usb_bus);
+
+    let mut usb_dev = { test.make_device(&usb_bus) };
+
+    loop {
+        if usb_dev.poll(&mut [&mut test]) {
+            test.poll();
+        }
+    }
+}
