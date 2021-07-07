91851964ff5da36d94f5e6d942c7fc7f3cf89602
Jonas Schievink <jonasschievink@gmail.com>
2021-07-07 14:36:46 +0200

    Address review comments

--- nrf-hal-common/src/usbd.rs
+++ nrf-hal-common/src/usbd.rs
@@ -1,18 +1,22 @@
+use core::marker::PhantomData;
+
 use crate::clocks::ExternalOscillator;
 use crate::pac::USBD;
 use crate::Clocks;
 
 pub use nrf_usbd::Usbd;
 
-#[allow(dead_code)] // fields are unused and only hold ownership
 pub struct UsbPeripheral<'a> {
-    usbd: USBD,
-    clocks: &'a (),
+    _usbd: USBD,
+    _clocks: PhantomData<&'a ()>,
 }
 
 impl<'a> UsbPeripheral<'a> {
     pub fn new<L, LSTAT>(usbd: USBD, _clocks: &'a Clocks<ExternalOscillator, L, LSTAT>) -> Self {
-        Self { usbd, clocks: &() }
+        Self {
+            _usbd: usbd,
+            _clocks: PhantomData,
+        }
     }
 }
 
