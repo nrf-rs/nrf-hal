b005d57ef479b655e9a9f40198775eaee55d9510
huntc <huntchr@gmail.com>
2021-07-17 12:38:05 +1000

    Missing xtask config

--- xtask/src/lib.rs
+++ xtask/src/lib.rs
@@ -27,6 +27,7 @@ pub static EXAMPLES: &[(&str, &[&str])] = &[
         "ppi-demo",
         &["51", "52810", "52811", "52832", "52833", "52840"],
     ),
+    ("nvmc-demo", &["52840"]),
     ("pwm-blinky-demo", &["52840"]),
     ("pwm-demo", &["52840"]),
     ("qdec-demo", &[]),
