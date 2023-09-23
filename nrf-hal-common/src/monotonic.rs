9d7a5ed055db89c5e5c908eb3ccb8019ca898b80
Kyrne <johnnylam0914@gmail.com>
2023-09-24 00:24:51 +0200

    first draft with now, clear_compare_flag, zero and reset

--- nrf-hal-common/src/monotonic.rs
+++ nrf-hal-common/src/monotonic.rs
@@ -1,92 +1,61 @@
 #[cfg(any(feature = "9160", feature = "5340-app", feature = "5340-net"))]
-use crate::pac::{
-    timer0_ns::{
-        RegisterBlock as RegBlock0, EVENTS_COMPARE, TASKS_CAPTURE, TASKS_CLEAR, TASKS_COUNT,
-        TASKS_START, TASKS_STOP,
-    },
-    Interrupt, TIMER0_NS as TIMER0, TIMER1_NS as TIMER1, TIMER2_NS as TIMER2,
-};
+use crate::pac::{rtc0_ns::RegisterBlock as rtcRegBlock, RTC0_NS as RTC0, RTC1_NS as RTC1};
 
 #[cfg(not(any(feature = "9160", feature = "5340-app", feature = "5340-net")))]
-use crate::pac::{
-    timer0::{
-        RegisterBlock as RegBlock0, EVENTS_COMPARE, TASKS_CAPTURE, TASKS_CLEAR, TASKS_COUNT,
-        TASKS_START, TASKS_STOP,
-    },
-    Interrupt, TIMER0, TIMER1, TIMER2,
-};
-use cast::u32;
-use embedded_hal::{
-    blocking::delay::{DelayMs, DelayUs},
-    prelude::*,
-    timer,
-};
-use nb::{self, block};
-use void::{unreachable, Void};
+use crate::pac::{rtc0::RegisterBlock as rtcRegBlock, RTC0, RTC1};
 
 #[cfg(any(feature = "52832", feature = "52833", feature = "52840"))]
-use crate::pac::{TIMER3, TIMER4};
-
-// The 832 and 840 expose TIMER3 and TIMER for as timer3::RegisterBlock...
-#[cfg(any(feature = "52832", feature = "52840"))]
-use crate::pac::timer3::{
-    RegisterBlock as RegBlock3, EVENTS_COMPARE as EventsCompare3, TASKS_CAPTURE as TasksCapture3,
-};
-
-// ...but the 833 exposes them as timer0::RegisterBlock. This might be a bug
-// in the PAC, and could be fixed later. For now, it is equivalent anyway.
-#[cfg(feature = "52833")]
-use crate::pac::timer0::{
-    RegisterBlock as RegBlock3, EVENTS_COMPARE as EventsCompare3, TASKS_CAPTURE as TasksCapture3,
-};
+use crate::pac::RTC2;
 
 use core::marker::PhantomData;
 use rtic_monotonic::Monotonic;
 
-pub trait RegisterAccess<RegBlock> {
-    fn reg<'a>() -> &'a RegBlock;
+pub trait InstanceRtc {
+    type RegBlock;
+    fn reg<'a>() -> &'a Self::RegBlock;
 }
 
-pub trait TimerRegister: RegisterAccess<RegBlock0> {}
-
-pub trait Instantiatable<RegBlock> {
-    fn new<Instance: RegisterAccess<RegBlock>>(instance: Instance) -> Self;
+pub struct MonotonicRtc<T: InstanceRtc, const FREQ: u32> {
+    instance: PhantomData<T>,
+    overflow: u8,
 }
 
-pub struct MonotonicTimer<Timer: TimerRegister> {
-    instance: PhantomData<Timer>,
-}
+impl<T: InstanceRtc, const FREQ: u32> MonotonicRtc<T, FREQ> {
+    fn new() {
+        todo!()
+    }
 
-impl<Timer: TimerRegister>  MonotonicTimer<Timer> {
-    pub fn new<Instance: RegisterAccess<RegBlock0>>(_: Instance) -> Self {
-        let reg = Timer::reg();
-        reg.prescaler.write(|w| unsafe { w.prescaler().bits(4) });
-        reg.bitmode.write(|w| w.bitmode()._32bit());
-        reg.tasks_start.write(|w| w.tasks_start().set_bit());
-        Self {
-            instance: PhantomData,
-        }
+    fn reg<'a>() -> &'a rtcRegBlock {
+        unsafe { &*RTC0::ptr() }
     }
 }
 
-impl<Timer: TimerRegister> Monotonic for MonotonicTimer<Timer> {
-    type Instant = fugit::TimerInstantU32<1_000_000>;
-    type Duration = fugit::TimerDurationU32<1_000_000>;
+impl<T: InstanceRtc, const FREQ: u32> Monotonic for MonotonicRtc<T, FREQ> {
+    type Instant = fugit::TimerInstantU32<FREQ>;
+    type Duration = fugit::TimerDurationU32<FREQ>;
 
     fn now(&mut self) -> Self::Instant {
-        let reg = Timer::reg();
-        reg.tasks_capture[0].write(|w| w.tasks_capture().set_bit());
-        let ticks = reg.cc[0].read().cc().bits();
-        let now = fugit::TimerInstantU32::from_ticks(ticks);
-        return now;
+        let rtc = Self::reg();
+        let cnt = rtc.counter.read().bits();
+
+        let ovf = (if rtc.events_ovrflw.read().bits() == 1 {
+            self.overflow.wrapping_add(1)
+        } else {
+            self.overflow
+        }) as u32;
+
+        Self::Instant::from_ticks((ovf << 24) | cnt)
     }
 
     fn set_compare(&mut self, instant: Self::Instant) {
-        Timer::reg().cc[0].write(|w| w.cc().variant(instant.duration_since_epoch().ticks()));
+        todo!()
     }
 
     fn clear_compare_flag(&mut self) {
-        Timer::reg().events_compare[0].write(|w| w.events_compare().clear_bit());
+        let rtc = Self::reg();
+        unsafe {
+            rtc.events_compare[0].write(|w| w.bits(0));
+        }
     }
 
     fn zero() -> Self::Instant {
@@ -94,37 +63,11 @@ impl<Timer: TimerRegister> Monotonic for MonotonicTimer<Timer> {
     }
 
     unsafe fn reset(&mut self) {
-        let reg = Timer::reg();
-        reg.intenset.write(|w| w.compare0().set_bit());
-        reg.tasks_clear.write(|w| w.tasks_clear().set_bit());
-        reg.tasks_start.write(|w| w.tasks_start().set_bit());
+        let rtc = Self::reg();
+        rtc.intenset.write(|w| w.compare0().set().ovrflw().set());
+        rtc.evtenset.write(|w| w.compare0().set().ovrflw().set());
+
+        rtc.tasks_clear.write(|w| w.bits(1));
+        rtc.tasks_start.write(|w| w.bits(1));
     }
 }
-
-
-
-
-macro_rules! reg_access {
-    ($(
-        $(#[$feature_gate:meta])?
-        $timer:ident
-    )+) => {
-        $(
-            $( #[$feature_gate] )?
-            impl RegisterAccess<RegBlock0> for $timer{
-                fn reg<'a>() -> &'a RegBlock0{
-                    unsafe {&*Self::ptr()}
-                }
-            }
-            $( #[$feature_gate] )?
-            impl TimerRegister for $timer{}
-            
-        )+
-        
-    };
-}
-reg_access!(
-    TIMER0
-    TIMER1
-    TIMER2
-);
