e7cf7e6e4b2b28c89b92d86cf833ea6e60756d37
Tyson Lawrence <tysonl@trace-eng.com>
2024-06-28 06:38:06 -0600

    Adding 9120 to blinky-button-test

--- examples/blinky-button-demo/src/main.rs
+++ examples/blinky-button-demo/src/main.rs
@@ -3,9 +3,17 @@
 
 use embedded_hal::digital::InputPin;
 use embedded_hal::digital::OutputPin;
-use nrf52832_hal as hal;
-use nrf52832_hal::gpio::Level;
 use rtt_target::{rprintln, rtt_init_print};
+
+#[cfg(feature = "52832")]
+use nrf52832_hal as hal;
+#[cfg(feature = "52832")]
+use nrf52832_hal::gpio::Level;
+
+#[cfg(feature = "9120")]
+use nrf9120_hal as hal;
+#[cfg(feature = "9120")]
+use nrf9120_hal::gpio::Level;
 
 #[panic_handler] // panicking behavior
 fn panic(_: &core::panic::PanicInfo) -> ! {
@@ -18,9 +26,20 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
 fn main() -> ! {
     rtt_init_print!();
     let p = hal::pac::Peripherals::take().unwrap();
+
+    #[cfg(feature = "52832")]
     let port0 = hal::gpio::p0::Parts::new(p.P0);
+    #[cfg(feature = "52832")]
     let mut button = port0.p0_13.into_pullup_input();
+    #[cfg(feature = "52832")]
     let mut led = port0.p0_17.into_push_pull_output(Level::Low);
+
+    #[cfg(feature = "9120")]
+    let port0 = hal::gpio::p0::Parts::new(p.P0_NS);
+    #[cfg(feature = "9120")]
+    let mut button = port0.p0_08.into_pullup_input();
+    #[cfg(feature = "9120")]
+    let mut led = port0.p0_00.into_push_pull_output(Level::Low);
 
     rprintln!("Blinky button demo starting");
     loop {
